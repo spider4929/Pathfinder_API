7c66d5ce5d5b02a5eb3de5f93cfadc7c2f606163
Kristian Baturiano <kjqb4929@gmail.com>
2023-04-16 16:55:40 +0800

    Reversed map simplification

--- pathfinder.py
+++ pathfinder.py
@@ -331,7 +331,7 @@ def pathfinder(source, goal, profile):
     weather_condition = api_response['weather'][0]['id']
 
     # retrieve map from database
-    graph = osmnx.graph_from_xml('marikina_complete.osm', simplify=True)
+    graph = osmnx.graph_from_xml('marikina_complete.osm', simplify=False)
 
     # get all edges for weight adjustment
     nodes, edges = osmnx.graph_to_gdfs(graph)
