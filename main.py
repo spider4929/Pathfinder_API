2959abfa22e0964394eef1ae8d598a12c397f3e7
Kristian Baturiano <kjqb4929@gmail.com>
2023-05-23 16:32:09 +0800

    feat: Changed finding of nearest edge
    Changed finding of nearest edge from pathfinding function to a route that
    report function can call. Edge details will now be saved in each report
    to remove load which lengthens query times in pathfinding function.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from pathfinder import pathfinder
+from pathfinder import pathfinder, get_nearest_edge
 from datetime import datetime
 
 app = Flask(__name__)
@@ -38,6 +38,22 @@ def find_path():
     print(f'/route/ Success - {now}')
 
     return route
+
+@app.route("/route/get_nearest_edge/", methods=["POST"])
+def nearest_edge():
+    args = request.get_json()
+    coords = args['coords']
+
+    y_coord = float(coords[1])
+    x_coord = float(coords[0])
+
+    edges = get_nearest_edge(y_coord, x_coord)
+
+    now = datetime.now()
+
+    print(f'route/get_nearest_edge/ Success - {now}')
+
+    return edges
 
 if __name__ == '__main__':
   app.run(port=5000)
