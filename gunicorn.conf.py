c849d52e532c3ede15fa7bf52d7293cbdb4f00bb
Kristian Baturiano <kjqb4929@gmail.com>
2023-10-24 23:23:42 +0800

    feat: Reduced number of workers to 1

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,9 +2,9 @@ def post_worker_init(worker):
     # Called just after a worker has been initialized
     worker.log.info("Worker spawned (pid: %s)", worker.pid)
 
-workers = 4
+workers = 1
 worker_class = "gthread"  # Use the 'gthread' worker class for better performance
-threads = 4  # Number of threads per worker
+threads = 1  # Number of threads per worker
 
 # Optional: If you're using any frameworks, you may want to tweak the worker timeout
 timeout = 120
